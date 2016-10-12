65d207ed37cb9d662467dae46b4e9bdc5b9b1cb6
IvanAlexandrovKolev <ivan.alexandrov.kolev@gmail.com>
2016-10-12 14:23:33 +0300

    added score and better escaping

--- Game.js
+++ Game.js
@@ -1,9 +1,8 @@
 function init() {
-    let moveSpeed = 4;
+    let moveSpeed = 1.5;
     let chickSpeedX = 2;
     let chickSpeedY = 2;
-    let dirX = true;
-    let dirY = true;
+    let score = 0;
     let marioImg = document.getElementById('mario');
     let chickImg = document.getElementById('chick');
     let princessImg = document.getElementById('princess');
@@ -11,7 +10,7 @@ function init() {
     ctx.font = '24px arial';
 
     let mario = {img:marioImg, x:400 , y:300,direction:true};
-    let chick = {img:chickImg, x:0 , y:0};
+    let chick = {img:chickImg, x:0 , y:0,dirX:true,dirY:true};
     let keysDown = {};
     window.addEventListener('keydown',kbdHandler);
     window.addEventListener('keyup',kbdHandler);
@@ -31,10 +30,12 @@ function init() {
     }
 
     function draw() {
-        ctx.fillRect(0,0,800,600);
+        ctx.clearRect(0,0,800,600);
         drawObj(mario);
         drawObj(chick);
         ctx.drawImage(princessImg,100,200);
+
+        ctx.fillText(`Chicks caught:${score}`,10,30);
     }
     function update() {
         if(keysDown["ArrowLeft"]){
@@ -56,32 +57,33 @@ function init() {
         let y = (mario.y + 37) - (chick.y+36);
         let distance = Math.sqrt(x*x + y*y);
         if (distance<50){
+            score++;
             reset();
         }
     }
     function moveChick() {
-        if(dirX){
+        if(chick.dirX){
             chick.x += chickSpeedX;
-            if(chick.x >= 770){
-                dirX = false;
+            if(chick.x >= 760){
+                chick.dirX = false;
             }
         }
         else{
             chick.x -= chickSpeedX;
-            if(chick.x <= 30){
-                dirX = true;
+            if(chick.x <= 1){
+                chick.dirX = true;
             }
         }
-        if(dirY){
+        if(chick.dirY){
             chick.y += chickSpeedY;
-            if(chick.y >= 570){
-                dirY = false;
+            if(chick.y >= 550){
+                chick.dirY = false;
             }
         }
         else{
             chick.y -= chickSpeedY;
-            if(chick.y <= 30){
-                dirY = true;
+            if(chick.y <= 1){
+                chick.dirY = true;
             }
         }
 
@@ -89,22 +91,39 @@ function init() {
         let y = (mario.y + 37) - (chick.y+36);
         let distance = Math.sqrt(x*x + y*y);
         //trying to make the ckick escape
-        if(chick.x < mario.x && distance <200){
-            chickSpeedX-= distance*0.0001;
+        if(chick.x < mario.x && distance <200 && chick.dirX){
+            chickSpeedX-= (200-distance)*0.0001;
         }
-        else chickSpeedX += 0.025;
-        if(chick.x > mario.x && distance <200){
-            chickSpeedX += distance*0.0001;
+        if(chick.x < mario.x && distance <200 && !chick.dirX){
+            chickSpeedX += (200-distance)*0.0001;
         }
-        else chickSpeedX -= 0.025;
-        if(chick.y < mario.x && distance <200){
-            chickSpeedY -= distance*0.0001;
+        if(chick.x > mario.x && distance <200 && chick.dirX){
+            chickSpeedX += (200-distance)*0.0001;
         }
-        else chickSpeedY += 0.025;
-        if(chick.y > mario.x && distance <200){
-            chickSpeedY += distance*0.0001;//
+        if(chick.x > mario.x && distance <200 && !chick.dirX){
+            chickSpeedX -= (200-distance)*0.0001;
         }
-        else chickSpeedY -= 0.025;
+
+        if(chick.y < mario.y && distance <200 && chick.dirY){
+            chickSpeedX-= (200-distance)*0.0001;
+        }
+        if(chick.y < mario.y && distance <200 && !chick.dirY){
+            chickSpeedX += (200-distance)*0.0001;
+        }
+        if(chick.y > mario.y && distance <200 && chick.dirY){
+            chickSpeedX -= (200-distance)*0.0001;
+        }
+        if(chick.y > mario.y && distance <200 && !chick.dirY){
+            chickSpeedX += (200-distance)*0.0001;
+        }
+        //if it goes out of screen
+        if(chick.x<0 || chick.x > 800 || chick.y <0 || chick.y >600 ){
+            chick.x = Math.round((Math.random()*700));
+            chick.y = Math.round((Math.random()*500));
+            chickSpeedX = 2;
+            chickSpeedY = 2;
+
+        }
     }
     function reset() {
         chick.x = Math.round((Math.random()*700));
