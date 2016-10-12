1e78b787696437344b8e9e8af315c469f9b87903
lyubozarMihovski <lyubozar@yahoo.com>
2016-10-12 21:09:23 +0300

    policeMan

--- Game.js
+++ Game.js
@@ -1,8 +1,9 @@
-
 function init() {
     let moveSpeed = 3.4;
     let chickSpeedX = 2;
     let chickSpeedY = 2;
+    let policemanSpeedX = 6;
+    let policemanSpeedY = 6;
     let score = 0;
     let level = 1;
     let marioImg = document.getElementById('mario');
@@ -10,34 +11,34 @@ function init() {
     let policeImg = document.getElementById('police');
     let portImg = document.getElementById('port');
     let canvas = document.getElementById('canvas');
-
+ 
     let ctx = canvas.getContext('2d');
     ctx.font = '18px arial';
     let mario = {img: marioImg, x: 400, y: 300, direction: true};
     let chicks = [];
-    let chick = {img: chickImg, x: 0, y: 0, dirX: true, dirY: true};
-
+    let policeman = {img: policeImg, x: 0, y: 0, dirX: true, dirY: true};
+ 
     let keysDown = {};
     window.addEventListener('keydown', kbdHandler);
     window.addEventListener('keyup', kbdHandler);
     canvas.addEventListener('click', mouseHandler);
-
-
+ 
+ 
     function kbdHandler() {
         if (event.type == 'keydown')
             keysDown[event.code] = true;
         else if (event.type == 'keyup')
             delete keysDown[event.code];
     }
-
+ 
     function mouseHandler(event) {
-        if(event.clientX>365 && event.clientX<446) {
+        if (event.clientX > 365 && event.clientX < 446) {
             if (event.clientY > 555 && event.clientY < 588) {
                 init();
             }
         }
     }
-
+ 
     function addChick() {
         let x = Math.round((Math.random() * 700));
         let y = Math.round((Math.random() * 500));
@@ -60,13 +61,13 @@ function init() {
         }
         chicks.push({img: chickImg, x: x, y: y, dirX: dirx, dirY: diry});
     }
-
+ 
     function drawObj(obj) {
         ctx.save();
         ctx.drawImage(obj.img, obj.x, obj.y);
         ctx.restore();
     }
-
+ 
     function draw() {
         ctx.clearRect(0, 0, 800, 600);
         ctx.drawImage(portImg, 30, 30);
@@ -74,17 +75,16 @@ function init() {
         ctx.drawImage(portImg, 740, 30);
         ctx.drawImage(portImg, 740, 540);
         drawObj(mario);
+        drawObj(policeman);
         for (let i = 0; i < chicks.length; i++) {
             drawObj(chicks[i]);
         }
-
-        ctx.drawImage(policeImg, 100, 200);
         ctx.fillText(`LEVEL: ${level}`, 300, 25);
         ctx.fillText(`CHICKS CAUGHT: ${score}`, 300, 50);
-
+ 
         drawButton('Reset', 350, 540);
     }
-
+ 
     function update() {
         if (keysDown["ArrowLeft"]) {
             mario.x -= moveSpeed;
@@ -100,8 +100,7 @@ function init() {
         if (keysDown["ArrowDown"]) {
             mario.y += moveSpeed;
         }
-
-
+ 
         //port
         if (((mario.x + 30) < 70 && (mario.x + 30) > 30) && ((mario.y + 37) < 70 && (mario.y + 37) > 30)) {
             let port = Math.round(Math.random() * 5);
@@ -123,7 +122,8 @@ function init() {
             if (port != 4)
                 teleport(port);
         }
-
+ 
+        movePoliceman(policeman);
         for (let i = 0; i < chicks.length; i++) {
             moveChick(chicks[i]);
             let x = (mario.x + 30) - (chicks[i].x + 22);
@@ -133,8 +133,9 @@ function init() {
                 reset(i);
             }
         }
+ 
     }
-
+ 
     function teleport(port) {
         if (port == 1) {
             mario.x = 80;
@@ -153,7 +154,39 @@ function init() {
             mario.y = 450;
         }
     }
-
+ 
+    function movePoliceman(policeman) {
+        if (policeman.dirX) {
+            policeman.x += policemanSpeedX;
+            if (policeman.x >= 760) {
+                policeman.dirX = false;
+                policeman.x = 750;
+            }
+        }
+        else {
+            policeman.x -= policemanSpeedX;
+            if (policeman.x <= 1) {
+                policeman.dirX = true;
+                policeman.x = 10;
+            }
+        }
+        if (policeman.dirY) {
+            policeman.y += policemanSpeedY;
+            if (policeman.y >= 550) {
+                policeman.dirY = false;
+                policeman.y = 540;
+            }
+        }
+        else {
+            policeman.y -= policemanSpeedY;
+            if (policeman.y <= 1) {
+                policeman.dirY = true;
+                policeman.y = 10;
+            }
+        }
+ 
+    }
+ 
     function moveChick(chick) {
         if (chick.dirX) {
             chick.x += chickSpeedX;
@@ -183,12 +216,12 @@ function init() {
                 chick.y = 10;
             }
         }
-
+ 
         let x = (mario.x + 30) - (chick.x + 22);
         let y = (mario.y + 37) - (chick.y + 36);
         let distance = Math.sqrt(x * x + y * y);
-
-
+ 
+ 
         //trying to make the chick escape
         if (chick.x < mario.x && distance < 200 && chick.dirX) {
             chickSpeedX -= (200 - distance) * 0.0001;
@@ -202,7 +235,7 @@ function init() {
         if (chick.x > mario.x && distance < 200 && !chick.dirX) {
             chickSpeedX -= (200 - distance) * 0.0001;
         }
-
+ 
         if (chick.y < mario.y && distance < 200 && chick.dirY) {
             chickSpeedX -= (200 - distance) * 0.0001;
         }
@@ -216,7 +249,7 @@ function init() {
             chickSpeedX += (200 - distance) * 0.0001;
         }
     }
-
+ 
     function drawButton(text, x, y) {
         ctx.save();
         ctx.strokeStyle = 'orange';
@@ -226,7 +259,7 @@ function init() {
         ctx.font = '24 px monospace';
         ctx.textAlign = 'center';
         ctx.textBaseline = 'middle';
-
+ 
         let width = ctx.measureText(text).width;
         ctx.beginPath();
         ctx.moveTo(x + 20, y + 20);
@@ -235,7 +268,7 @@ function init() {
         ctx.fillText(text, x + 20 + width / 2, y + 20);
         ctx.restore();
     }
-
+ 
     function reset(number) {
         chicks.splice(number, 1);
         score++;
@@ -245,20 +278,20 @@ function init() {
             moveSpeed -= 0.2;
         }
         if (chicks.length == 0) {
-            for (let i = 0; i < (level)/2; i++) {
+            for (let i = 0; i < level; i++) {
                 addChick();
             }
         }
     }
-
+ 
     function main() {
         update();
         draw();
         requestAnimationFrame(main);
     }
-
+ 
     addChick();
     main();
 }
-
+ 
 init();
