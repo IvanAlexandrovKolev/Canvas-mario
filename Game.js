424b2ad45816e0423b4ab62e613d05e4f920dde2
Martin <mrindzhov@gmail.com>
2016-10-12 18:43:29 +0300

    Added drawButton function

--- Game.js
+++ Game.js
@@ -202,6 +202,23 @@ function init() {
             chickSpeedX += (200-distance)*0.0001;
         }
     }
+    function drawButton(text,x,y) {
+        let ctx=document.getElementById('canvas').getContext('2d');
+        ctx.strokeStyle ='orange';
+        ctx.lineCap='round';
+        ctx.lineWidth=40;
+        ctx.fillStyle='white';
+        ctx.font='24 px monospace';
+        ctx.textAlign='center';
+        ctx.textBaseline='middle';
+
+        let width=ctx.measureText(text).width;
+        ctx.beginPath();
+        ctx.moveTo(x+20,y+20);
+        ctx.lineTo(x+20+width,y+20);
+        ctx.stroke();
+        ctx.fillText(text,x+20+width/2,y+20);
+    }
     function reset(number) {
         chicks.splice(number,1);
         score++;
